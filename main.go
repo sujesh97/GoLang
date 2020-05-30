fd2c4cf5fc60d4ada437a7801806192a1b7c0430
sujesh <sujesharadhya97@gmail.com>
2020-05-30 13:53:07 +0530

    code1

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Response struct {
+	Name      string      `json:"name"`
+	Character []Character `json:"character"`
+}
+
+type Character struct {
+	CharName string `json:"name"`
+	MaxPower int    `json:"max_power"`
+}
 
 func main() {
-	fmt.Print("hello")
+
+	response, err := http.Get("http://www.mocky.io/v2/5ecfd5dc3200006200e3d64b")
+	if err != nil {
+		fmt.Println("request failed with error %s \n", err)
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Println("request failed with error %s \n", err)
+	}
+	//fmt.Println(string(data))
+
+	var responseObject Response
+	json.Unmarshal(data, &responseObject)
+
+	fmt.Println(responseObject.Name)
+	fmt.Println(len(responseObject.Character))
+
+	for i := 0; i < len(responseObject.Character); i++ {
+		fmt.Println(responseObject.Character[i].CharName, responseObject.Character[i].MaxPower)
+	}
+
 }
